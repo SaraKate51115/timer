358a6afbfef04d09efaac46f007bf265b1ea6962
SaraKate51115 <sarakate51115@gmail.com>
2020-11-04 00:37:43 +0000

    created a timer to beep an unlimited number of alarms using command line arguments

---
+++ timer1.js
@@ -0,0 +1,20 @@
+let times = process.argv.slice(2);
+
+for (let time of times) {
+
+  if (time < 0) {
+
+    setTimeout(() => {
+      process.stdout.write(`no beep :( for: ${time}   `);
+    }, 250);
+
+  } else {
+
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, 250);
+  
+  }
+}
+  
+
